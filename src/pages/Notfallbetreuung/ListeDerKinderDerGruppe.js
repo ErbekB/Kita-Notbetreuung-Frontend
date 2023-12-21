faad725a0c3533a57ef56a89dee318df6f803568
JulianSoe <j.soe@gmx.de>
2023-12-21 16:46:03 +0100

    Button für Teilnahme an Notbetreuung implementiert und Änderung der Anzeige

--- src/pages/Notfallbetreuung/ListeDerKinderDerGruppe.js
+++ src/pages/Notfallbetreuung/ListeDerKinderDerGruppe.js
@@ -1,53 +1,59 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import async from "async";
 
 function ListeDerKinderDerGruppe() {
     const [data, setData] = useState([])
     const [teilnahme, setTeilnahme] = useState([])
     /*const {user} = props*/
     useEffect(() => {
-        return function fetchData() {
-            axios.get('http://localhost:8080/notfall/1')
-                .then(response => {
-                    setData(response.data.kinder);
-                    console.log("hallo")
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/notfall/1')
+
+                setData(response.data.kinder);
+                console.log("hallo");
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         };
+        fetchData();
     }, []);
 
 
-const handleClick = async () => {
-   /* try {
-        const response = await axios.post('http://localhost:8080//notfall/1', {
-            kindId: id,
-
-        });
-
-        setTeilnahme([...teilnahme, response.data]);
-
-        console.log(`Notbetreuung für Kind ${id} in Anspruch genommen.`);
-    } catch (error) {
-        console.error('Error adding Teilnahme:', error);
-    }*/
-};
-return (
-
-        <div>{data.map((kind, index) => (
-            <p key={index}> {kind.vorname}
-                { (1+1) === kind.id &&
-                    < button onClick={()=>handleClick()}>Notbetreuung für Kind {kind.vorname} in Anspruch
-                        nehmen</button>}
-
-            </p> //name={item.name} url={item.url}
-        ))}
+    const handleClick = async (kindId) => {
+        try {
+            axios.post(`http://localhost:8080/notfall/${kindId}`);
+            const updatedData = data.map((kind) =>
+                kind.id === kindId ? {...kind, teilnahmeNotbetreuung: true} : kind
+            );
+            setData(updatedData);
+        } catch (error) {
+            console.error('Error toggling Notbetreuung:', error);
+        }
+    };
+    return (
+        <div>
+            <div>{data.map((kind, index) => (
+                kind.teilnahmeNotbetreuung === true && <p key={index}>{kind.vorname}</p>
+            ))}
+            </div>
+            <hr/>
+            <div>{data.map((kind, index) => (
+                    kind.teilnahmeNotbetreuung === false &&
+                    (<p key={index}> {kind.vorname}
+                            {(1 + 1) === kind.id && (
+                                < button onClick={() => handleClick(kind.id)}>Notbetreuung für Kind {kind.vorname} in
+                                    Anspruch
+                                    nehmen</button>)}
+                        </p>
+                    )
+                )
+            )
+            }
+            </div>
         </div>
-
-    )
-
-
+    );
 }
 
 export default ListeDerKinderDerGruppe;
