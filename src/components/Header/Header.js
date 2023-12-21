c4ab0b8d75768b8c918350115a71d1f21e673a4e
Fatih Baskurt <baskurt.f@hotmail.de>
2023-12-21 16:03:42 +0100

    Notbetreuung wurde aus der NavBar in die Home.js versetzt. Notbetreuung An-Ausschalten wurde implementiert.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 function Header() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        return async function fetchData() {
+            await axios.get('http://localhost:8080/index/11')
+                .then(response => {
+                    setData(response.data);
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        };
+    }, []);
+
 
     return (
         <div>
             <header>
                 <h2>Kitaorganisation</h2>
-                <h3>Gruppe</h3>
             </header>
             <div>
                 <div className="navButtons">
-                    <a href="../Notfallbetreuung"><button className="navItem">Notbetreuung verwalten</button></a>
-                    <a href="../"><button className="navItem">Home</button></a>
-                    <a href="../Admin"><button className="navItem">Admin</button></a>
-                    <a href="../Logout"><button className="navItem">Logout</button></a>
+                    <a href="../Notfallbetreuung">
+                        <button className="navItem">Notbetreuung verwalten</button>
+                    </a>
+                    <a href="../">
+                        <button className="navItem">Home</button>
+                    </a>
+                    {data.admin ? <a href="../Admin">
+                        <button className="navItem">Admin</button>
+                    </a> : ""}
+                    <a href="../Logout">
+                        <button className="navItem">Logout</button>
+                    </a>
                 </div>
             </div>
         </div>
