368bc72a99ef648cb74e882f6d56855a35d9559a
ErbekB <145931150+ErbekB@users.noreply.github.com>
2023-12-26 11:54:02 +0100

    Header und Footer werden nur angezeigt, wenn der Benutzer eingeloggt ist.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter as Router, Route, Routes, useNavigate} from 'react-router-dom';
 import './App.css';
 
@@ -8,22 +8,25 @@ import Admin from './pages/Admin/Admin';
 import Login from './pages/Login/Login';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import AuthCheck from "./pages/Login/AuthCheck";
 
 function App() {
     const [istEingeloggt, setIstEingeloggt] = useState(false);
 
     return (
         <div className="App">
-            {istEingeloggt && <Header/>}
             <Router>
+                <AuthCheck setIstEingeloggt={setIstEingeloggt}/>
+                {istEingeloggt && <Header/>}
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/Notfallbetreuung" element={<Notfallbetreuung/>}/>
                     <Route path="/admin" element={<Admin/>}/>
-                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/login" element={<Login istEingeloggt={istEingeloggt} setIstEingeloggt={setIstEingeloggt}/>}/>
                 </Routes>
+                {istEingeloggt && <Footer/>}
             </Router>
-            {istEingeloggt && <Footer/>}
+
         </div>
     );
 }
