3e5c52be163deea2e84fd88cf438172127ef82a3
ErbekB <145931150+ErbekB@users.noreply.github.com>
2023-12-26 17:22:29 +0100

    Der Header wurde Css hinzugefügt. Die Componenten wurden angepasst.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,58 +1,73 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
-import async from "async";
+import {Link, useNavigate, useLocation} from "react-router-dom";
 
 function Header() {
-    const [data, setData] = useState([]);
+    const [daten, setDaten] = useState([]);
+    const [aktivesMenue, setAktivesMenue] = useState("home");
     const navigate = useNavigate();
+    const location = useLocation();
 
     useEffect(() => {
-        return async function fetchData() {
-            await axios.get("http://localhost:8080/index/11")
-                .then(response => {
-                    setData(response.data);
-                })
-                .catch(error => {
-                    console.error("Error beim laden der Daten:", error);
-                });
+        const datenAbrufen = async () => {
+            try {
+                const response = await axios.get("http://localhost:8080/index/11");
+                setDaten(response.data);
+            } catch (error) {
+                console.error("Fehler beim Laden der Daten:", error);
+            }
         };
-    }, []);
 
-    const logout = async () => {
+        datenAbrufen();
+
+        const pfad = location.pathname;
+        const aktuellesMenue = pfad.includes("/Admin") ? "admin" :
+            pfad.includes("/Notfallbetreuung") ? "notbetreuung" : "home";
+        setAktivesMenue(aktuellesMenue);
+    }, [location.pathname]);
+
+    const abmelden = async () => {
         try {
-            await axios.post("http://localhost:8080/logout",
-                {},
-                {withCredentials: true });
-            navigate(("/login"))
+            await axios.post("http://localhost:8080/logout", {}, {withCredentials: true});
+            navigate("/login");
         } catch (error) {
-            console.error("Logout Fehler:", error)
+            console.error("Fehler beim Abmelden:", error);
+            // Optional: Benutzerfeedback hinzufügen
         }
-
-    }
-
+    };
 
     return (
         <div>
             <header>
                 <h2>Kitaorganisation</h2>
-            </header>
-            <div>
-                <div className="navButtons">
-                    <a href="../Notfallbetreuung">
-                        <button className="navItem">Notbetreuung verwalten</button>
-                    </a>
-                    <a href="../">
-                        <button className="navItem">Home</button>
-                    </a>
-                    {data.admin ? <a href="../Admin">
-                        <button className="navItem">Admin</button>
-                    </a> : ""}
-                    <a href="../Logout">
-                        <button className="navItem" onClick={logout}>Logout</button>
-                    </a>
+                <div className="navCenter">
+                    <div className="navButtons">
+                        <Link to="/">
+                            <button className={`navItem ${aktivesMenue === "home" ? "active" : ""}`}
+                                    onClick={() => setAktivesMenue("home")} data-label="Home">
+                                <i className="fas fa-home"></i>
+                            </button>
+                        </Link>
+                        <Link to="/Notfallbetreuung">
+                            <button className={`navItem ${aktivesMenue === "notbetreuung" ? "active" : ""}`}
+                                    onClick={() => setAktivesMenue("notbetreuung")} data-label="Notbetreuung">
+                                <i className="fas fa-briefcase-medical"></i>
+                            </button>
+                        </Link>
+                        {daten.admin && (
+                            <Link to="/Admin">
+                                <button className={`navItem ${aktivesMenue === "admin" ? "active" : ""}`}
+                                        onClick={() => setAktivesMenue("admin")} data-label="Admin">
+                                    <i className="fas fa-user-cog"></i>
+                                </button>
+                            </Link>
+                        )}
+                    </div>
                 </div>
-            </div>
+                <button className="logoutButton" onClick={abmelden}>
+                    <i className="fas fa-sign-out-alt"></i>
+                </button>
+            </header>
         </div>
     );
 }
