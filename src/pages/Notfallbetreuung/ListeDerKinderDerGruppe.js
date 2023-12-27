b8555a28556955fde737eba2e05a6bd78abb6b9f
JulianSoe <j.soe@gmx.de>
2023-12-27 14:34:29 +0100

    temporäres entfernen aus der Kitagruppe hinzugefügt

--- src/pages/Notfallbetreuung/ListeDerKinderDerGruppe.js
+++ src/pages/Notfallbetreuung/ListeDerKinderDerGruppe.js
@@ -5,13 +5,16 @@ import async from "async";
 function ListeDerKinderDerGruppe() {
     const [data, setData] = useState([])
     const [id, setId] = useState(0)
+
     /*const {user} = props*/
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get('http://localhost:8080/notfall', {withCredentials: true})
-                setId(response.data.userId)
+                setId(response.data.userId);
                 setData(response.data.kinder);
+
+                console.log(data)
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -20,15 +23,15 @@ function ListeDerKinderDerGruppe() {
     }, []);
 
 
-    function notbetreuungTeilnehmen (kindId) {
+    function notbetreuungTeilnehmen(kindId) {
         try {
             axios.post(`http://localhost:8080/notfall/${kindId}`);
             const updatedData = data.map((kind) =>
-                kind.id === kindId ? {...kind, teilnahmeNotbetreuung: true, counter : kind.counter +1} : kind
+                kind.id === kindId ? {...kind, teilnahmeNotbetreuung: true, counter: kind.counter + 1} : kind
             );
             setData(updatedData);
             console.log(data)
-            } catch (error) {
+        } catch (error) {
             console.error('Error', error);
         }
     };
@@ -47,18 +50,19 @@ function ListeDerKinderDerGruppe() {
     }
 
 
-    function teilnahmeAendern(kindId) {
+    function teilnahmeNichtNotwendig(kindId) {
         const bestaetigen = window.confirm("Bitte bestätige die nicht-Teilnahme deines Kindes. Eine Änderung ist dannach nur noch durch den Administrator möglich");
-        if (bestaetigen){
-        try {
-            axios.post(`http://localhost:8080/notfall/teilnahme/${kindId}`);
-            const temporalData = data.filter((kind) => kind.id !== kindId);
-            setData(temporalData);
-        } catch (error) {
-            console.error('Error', error);
-        }
-        }
-        else {
+        if (bestaetigen) {
+            try {
+                axios.post(`http://localhost:8080/notfall/teilnahme/${kindId}`);
+                const updatedData = data.map((kind) =>
+                    kind.id === kindId ? {...kind, notbetreuungNichtNotwendig: true} : kind
+                );
+                setData(updatedData);
+            } catch (error) {
+                console.error('Error', error);
+            }
+        } else {
 
         }
     }
@@ -68,24 +72,29 @@ function ListeDerKinderDerGruppe() {
             <div><h2>
                 an Notbetreuung teilnehmend:
             </h2>{data.map((kind, index) => (
-                kind.teilnahmeNotbetreuung === true && <p key={index}>{kind.vorname} bisherige Teilnahmen{kind.counter}<button onClick={() => nichtTeilnehmen(kind.id)}>Teilnahme zurückziehen </button></p>
+                kind.teilnahmeNotbetreuung === true && <p key={index}>{kind.vorname} bisherige Teilnahmen{kind.counter}
+                    <button onClick={() => nichtTeilnehmen(kind.id)}>Teilnahme zurückziehen</button>
+                </p>
             ))}
             </div>
             <hr/>
             <div><h2>
-            Kinder der Gruppe
+                Kinder der Gruppe
             </h2>
                 {data.map((kind, index) => (
-                    kind.teilnahmeNotbetreuung === false && <p key={index}> {kind.vorname} bisherige Teilnahmen: {kind.counter}
-                            {(id) === kind.id &&
-                                <button onClick={() => notbetreuungTeilnehmen(kind.id)}>Notbetreuung für Kind {kind.vorname} in
-                                    Anspruch
-                                    nehmen
-                                </button>}
+
+                    kind.teilnahmeNotbetreuung === false && kind.notbetreuungNichtNotwendig === false &&
+                    <p key={index}> {kind.vorname} bisherige Teilnahmen: {kind.counter}
                         {(id) === kind.id &&
-                        <button onClick={() => teilnahmeAendern(kind.id)}>nicht teilnehmen</button>}
-                                </p>
-                                ))
+                            <button onClick={() => notbetreuungTeilnehmen(kind.id)}>Notbetreuung für
+                                Kind {kind.vorname} in
+                                Anspruch
+                                nehmen
+                            </button>}
+                        {(id) === kind.id &&
+                            <button onClick={() => teilnahmeNichtNotwendig(kind.id)}>nicht teilnehmen</button>}
+                    </p>
+                ))
                 }
             </div>
         </div>
