87f442ed38cbac28592d94c59db9931e156d12e4
Fatih Baskurt <baskurt.f@hotmail.de>
2023-12-28 15:47:30 +0100

    Registrieren und Login wurden gestylt. Logo wurde erstellt.

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,9 @@
+// Deine React-Komponente
 import React, {useEffect, useState} from 'react';
 import './Login.css';
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import KitaImage from '../../images/Logo.png';
 
 function Login({istEingeloggt, setIstEingeloggt}) {
     const [name, setName] = useState("");
@@ -22,7 +24,7 @@ function Login({istEingeloggt, setIstEingeloggt}) {
             const response = await axios.post("http://localhost:8080/login", {
                 name: name,
                 passwort: passwort
-            }, { withCredentials: true });
+            }, {withCredentials: true});
             setIstEingeloggt(true);
             navigate("/")
         } catch (error) {
@@ -31,30 +33,58 @@ function Login({istEingeloggt, setIstEingeloggt}) {
     };
 
     return (
-        <div className="Login">
-            <h1>Login</h1>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label>Benutzername:</label>
-                    <input
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                    />
+        <div className="Login-Body">
+            <div className="Login-Container">
+                <div className="Logo">
+                    <img src={KitaImage} alt="Kita-Bild" className="KitaImage"/>
                 </div>
-                <div>
-                    <label>Passwort:</label>
-                    <input
-                        type="password"
-                        value={passwort}
-                        onChange={(e) => setPasswort(e.target.value)}
-                        required
-                    />
+                <div className="KitaTitle">
+                    Kita-Notbetreuung
                 </div>
-                <button type="submit">Einloggen</button>
-                {loginError && <p className="error">{loginError}</p>}
-            </form>
+                <div className="KitaDescription">
+                    Mit Kita-Notbetreuung bist du und dein Kind auf der sicheren Seite.
+                    <br/>Worauf wartest du noch?
+                    <br/>Organisier deine Notbetreuung kinderleicht!
+                </div>
+                <br/>
+                <br/>
+                <div className="Login">
+                    <form onSubmit={handleLogin}>
+                        <div className="Benutzername">
+                            <label>
+                                <input
+                                    type="text"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    placeholder="Benutzername"
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <br/>
+                        <div className="Passwort">
+                            <label>
+                                <input
+                                    type="password"
+                                    value={passwort}
+                                    onChange={(e) => setPasswort(e.target.value)}
+                                    placeholder="Passwort"
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <br/>
+                        <button className="Anmeldeknopf" type="submit">Anmelden</button>
+                        {loginError && <p className="error">{loginError}</p>}
+                        <div>
+                            <br/>
+                            Noch kein Benutzer? Hier geht es zur <Link to="/registrieren">Registrierung</Link>.
+                        </div>
+
+                    </form>
+                    <br/>
+                </div>
+            </div>
         </div>
     );
 }
