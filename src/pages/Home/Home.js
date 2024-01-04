c737f442a1df7f54965dc8b34198bfc7db1cbbe4
ErbekB <145931150+ErbekB@users.noreply.github.com>
2024-01-04 11:43:15 +0100

    Revert "Historie implementiert"

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,9 +5,8 @@ import axios from 'axios';
 function Home() {
     const [data, setData] = useState([]);
     const [admin, setAdmin] = useState();
-    const [notbetreuung, setNotbetreuung] = useState(false);
+    const [notbetreuung, setNotbetreuung] = useState();
     const [verlauf, setVerlauf] = useState([]);
-    const [anzeigen, setAnzeigen] = useState(false);
 
 
     useEffect(() => {
@@ -22,94 +21,88 @@ function Home() {
             }
         }
 
-        async function statusVerlauf() {
-            try {
-                const response = await axios.get('http://localhost:8080/verlauf', {withCredentials: true});
-                setVerlauf(response.data.verläufe);
-            } catch (error) {
-                console.error('Error fetching Statusverlauf:', error);
-            }
-        }
-
         fetchData();
-        statusVerlauf();
-    },);
 
-    const toggleNotbetreuung = async () => {
-        try {
-            await axios.post('http://localhost:8080/index', '', {withCredentials: true});
-            setNotbetreuung((notbetreuung) => !notbetreuung);
-        } catch (error) {
-            console.error('Fehler beim Umschalten der Notbetreuung:', error);
-        }
-    };
+        axios.get("http://localhost:8080/verlauf", {withCredentials: true})
+            .then(response => {
+                setVerlauf(response.data.verläufe.kinder);
+                console.log(verlauf)
+            })
+            .catch (error =>{
+            console.error('Error fetching data:', error);
+        })
+    }, []);
 
-    data.sort((a, b) => a.counter - b.counter);
-
-
-    function aufklappen() {
-        setAnzeigen(!anzeigen);
+const toggleNotbetreuung = async () => {
+    try {
+        await axios.post('http://localhost:8080/index', '', {withCredentials: true});
+        setNotbetreuung((notbetreuung) => !notbetreuung);
+    } catch (error) {
+        console.error('Fehler beim Umschalten der Notbetreuung:', error);
     }
+};
 
-    return (
-        <div className="home-body">
-            <div className="home-container">
-                <h1 className="home-title">Startseite</h1>
-                <h2 className={notbetreuung ? "notbetreuung-anzeige red-text" : "notbetreuung-anzeige standard-text"}>
-                    {notbetreuung ? "Morgen ist Notbetreuung" : "Keine Notbetreuung"}
-                </h2>
-                {admin ? <button onClick={toggleNotbetreuung}>Notbetreuung</button> : ''}
-                <br/>
-                <table className="kindergruppe-table">
-                    <thead>
-                    <tr>
-                        <th className="kind">Name</th>
-                        <th className="kind">Nachname</th>
-                        <th className="teilnahmen">Teilnahme/n</th>
-                        <th className="punkt"></th>
+data.sort((a, b) => a.counter - b.counter);
+
+
+return (
+    <div className="home-body">
+        <div className="home-container">
+            <h1 className="home-title">Startseite</h1>
+            <h2 className={notbetreuung ? "notbetreuung-anzeige red-text" : "notbetreuung-anzeige standard-text"}>
+                {notbetreuung ? "Morgen ist Notbetreuung" : "Keine Notbetreuung"}
+            </h2>
+            {admin ? <button onClick={toggleNotbetreuung}>Notbetreuung</button> : ''}
+            <br/>
+            <table className="kindergruppe-table">
+                <thead>
+                <tr>
+                    <th className="kind">Name</th>
+                    <th className="kind">Nachname</th>
+                    <th className="teilnahmen">Teilnahme/n</th>
+                    <th className="punkt"></th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map((kind, index) => (
+                    <tr key={index}>
+                        <td className="kind">{kind.vorname}</td>
+                        <td className="kind">{kind.nachname}</td>
+                        <td className="teilnahmen">{kind.counter}</td>
+                        {index < 5 && (
+                            <td className="punkt">🟢</td>
+                        )}
+                        {index >= 5 && index < 8 && (
+                            <td className="punkt">🟡</td>
+                        )}
+                        {index >= 8 && (
+                            <td className="punkt">🔴</td>
+                        )}
                     </tr>
-                    </thead>
-                    <tbody>
-                    {data.map((kind, index) => (
-                        <tr key={index}>
-                            <td className="kind">{kind.vorname}</td>
-                            <td className="kind">{kind.nachname}</td>
-                            <td className="teilnahmen">{kind.counter}</td>
-                            {index < 5 && (
-                                <td className="punkt">🟢</td>
-                            )}
-                            {index >= 5 && index < 8 && (
-                                <td className="punkt">🟡</td>
-                            )}
-                            {index >= 8 && (
-                                <td className="punkt">🔴</td>
-                            )}
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-                <br/>
-                <table>
-                    <thead>
-                    <tr>
-                        <th className="historie">Historie</th>
+                ))}
+                </tbody>
+            </table>
+            <br/>
+            {verlauf && (
+            <table>
+                <thead>
+                <tr>
+                    <th className="datum">Datum</th>
+                    <th className="kinder">Teilgenommen</th>
+                </tr>
+                </thead>
+                <tbody>
+                {verlauf.map((eintrag, index) => (
+                    <tr key={index}>
+                      <td className="datum">{eintrag.datum}</td>
+                      <td className="kinder">{eintrag.kinder}</td>
                     </tr>
-                    </thead>
-                    <tbody>
-                    {verlauf.map((eintrag) => (
-                        <tr>
-                            <td>
-                                <span className="datum" onClick={aufklappen}>{eintrag.datum}</span>
-                                {anzeigen && <span>{eintrag.kinder.map((kind) => (
-                                    <p>{kind.vorname} {kind.nachname}</p>))}</span>}
-                            </td>
-                        </tr>
                     ))}
-                    </tbody>
-                </table>
-            </div>
+                </tbody>
+            </table>)}
         </div>
-    );
+    </div>
+);
 }
 
 
